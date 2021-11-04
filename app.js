e244c601d56116949f256946f9f021b89c42df01
justindevelops <justinsholt42@gmail.com>
2021-11-04 04:31:16 -0600

    updated page layout, added lots of content files, included background, updated CSS and app.js

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const temperature = document.querySelector('.temperature');
-const icon = document.querySelector('img');
+const tempDisplay = document.querySelector('#temperature');
+//const icon = document.querySelector('.midImage');
 const API_KEY = "b5e108b133efdb36384b821ad7ad5315";
 const submitButton = document.querySelector('.submit-btn');
+
+const topTitle = document.querySelector('.topInfo h1');
+const topCityName = document.querySelector('.topInfo h5');
+const topDateAndTime = document.querySelector('.topInfo h6');
+
+const cellFeelsLike = document.querySelector('.feelsLike h1');
+const cellHumidity = document.querySelector('.humidity h1');
+const cellRainChance = document.querySelector('.rainChance h1');
+const cellWindSpeed = document.querySelector('.windSpeed h1');
+
 let cityName = "Denver"; //default
-let fahrenheit = 0;
+let temperature = 0;
+
+//weather object
+function Weather(temperature, feelsLike, humidity, windSpeed, description) {
+  this.temperature = temperature;
+  this.feelsLike = feelsLike;
+  this.humidity = humidity;
+  this.windSpeed = windSpeed;
+  this.description = description;
+}
 
 //check URL for pre-defined parameters
 const queryString = window.location.search;
@@ -19,26 +38,51 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&APPID='
         return response.json();
       })
       .then(function(response) {
-          fahrenheit = Math.round(response.main.temp * (9 / 5) + 32);
+          temperature = Math.round(response.main.temp * (9 / 5) + 32);
+          console.log(response);
+          let feelsLike = Math.round(response.main.feels_like * (9 / 5) + 32);
+          let humidity = response.main.humidity;
+          let windSpeed = response.wind.speed;
+          let description = response.weather[0].main;
+          const weather = new Weather(temperature, feelsLike, humidity, windSpeed, description);
           
           //sends the current weather status (i.e. "Clouds", "Clear", "Rain")
           //to the updatePage function
-          updatePage(response.weather[0].main);
-          temperature.innerHTML = fahrenheit;
-          console.log(response);
-          console.log(response.weather[0].main)
+          updatePage(weather);
+          //tempDisplay.innerHTML = temperature;
+          //console.log(response);
+          //console.log(response.weather[0].main)
       })
       .catch(function(err) {
         console.log(err);
-
       });
 
 let updatePage = function(weather) {
-  updateIcon(weather);
+  //updateIcon(weather.description);
+
+  //update the three lines at the top of the page
+  topTitle.innerHTML = weather.description;
+  topCityName.innerHTML = cityName;
+  let today = new Date();
+  let date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+  topDateAndTime.innerHTML = date
+
+  //update the cells on left and right side of page
+  cellFeelsLike.innerHTML = weather.feelsLike + "°";
+  cellHumidity.innerHTML = weather.humidity + "%";
+  //cellRainChance.innerHTML = weather.rainChance;
+  cellWindSpeed.innerHTML = weather.windSpeed + "<br>km/h";
+
+  const node = document.createElement("div");
+  node.id = "temperature";
+  node.innerHTML = "The current temperature in " + cityName + " is " + weather.temperature + "°.";
+  let midContainer = document.querySelector('.midcontainer');
+  midContainer.appendChild(node);
+  tempDisplay.innerHTML = temperature;
 }
 
-let updateIcon = function(weather) {
-  icon.src="content/icons/" + weather + ".png";
+let updateIcon = function(description) {
+  icon.src="content/icons/" + description + ".png";
 }
 
 submitButton.addEventListener("click", function() {
